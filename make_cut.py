9467deacefd786d5e054ba03dad0f304d198033e
catohaste <catohaste@users.noreply.github.com>
2021-06-07 12:46:50 +0100

    Reduce storage of parameters. Add in Vg1/bmp4 regulation.

--- make_cut.py
+++ make_cut.py
@@ -29,8 +29,8 @@ now = datetime.now()
 date_time_str = now.strftime("%Y_%m_%d_%H%M")
 
 # save_directory = date_time_str
-save_directory = 'results/748cells/'
-# save_directory = 'testing/'
+# save_directory = 'results/748cells/'
+save_directory = 'results/testing/'
 if not os.path.isdir(save_directory):
     os.mkdir(save_directory)
 
@@ -39,8 +39,8 @@ if not os.path.isdir(save_directory):
 
 # number_of_cells_precut = 100
 # dx = 0.1
-# left_post_cut = 35
-# right_post_cut = 24
+# left_post_cut = 15
+# right_post_cut = 15
 # dt = 0.0001
 # sample_rate = 200
 
@@ -65,7 +65,8 @@ k = params["k"]
 k_v = params["k_v"]
 mu = params["mu"]
 lambda_const = params["lambda_const"]
-lambda_const_v = params["lambda_const_v"]
+lambda_const_v_0 = params["lambda_const_v_0"]
+lambda_const_v_b = params["lambda_const_v_b"]
 
 ########################################################################################
 """ Set desired timepoints """
@@ -77,11 +78,17 @@ postcut_time = total_time - cut_time
 
 t_precut = np.append(np.arange(0, cut_time, dt), cut_time)
 number_of_timepoints_precut = len(t_precut)
-# print(len(t_precut))
+store_t_precut = t_precut[::sample_rate]
+store_timepoints_precut = len(store_t_precut)
+# print(number_of_timepoints_precut, store_timepoints_precut)
+# print(store_t_precut[-1])
 
 t_postcut = np.append(np.arange(cut_time, total_time, dt), total_time)
 number_of_timepoints_postcut = len(t_postcut)
-# print(len(t_postcut))
+store_t_postcut = t_postcut[::sample_rate]
+store_timepoints_postcut = len(store_t_postcut)
+# print(number_of_timepoints_postcut, store_timepoints_postcut)
+# print(store_t_postcut[-1])
 
 
 ########################################################################################
@@ -101,11 +108,10 @@ for i in range(int(number_of_cells_precut/2)):
 
 start = time.time()
 
-a_precut, b_precut, c_precut, v_precut = solve_ivp_odes(number_of_cells_precut, number_of_timepoints_precut, dt, dx, a0, b0, c0, v0, calcium_periodic_boundary, params)
+a_precut, b_precut, c_precut, v_precut = solve_ivp_odes(number_of_cells_precut, number_of_timepoints_precut, dt, dx, a0, b0, c0, v0, calcium_periodic_boundary, params, sample_rate)
 
 end = time.time()
 print("Time consumed solving precut: ",end - start)
-
 
 ########################################################################################
 """ MAKE CUT AND SOLVE """
@@ -119,10 +125,11 @@ c_atcut = [c_precut[ant_idx, -1] for ant_idx in anterior_half]
 v_atcut = [v_precut[ant_idx, -1] for ant_idx in anterior_half]
 
 number_of_cells_postcut = len(anterior_half)
+print(anterior_half, number_of_cells_postcut)
 
 start = time.time()
 
-a_postcut_short, b_postcut_short, c_postcut_short, v_postcut_short = solve_ivp_odes(number_of_cells_postcut, number_of_timepoints_postcut, dt, dx, a_atcut, b_atcut, c_atcut, v_atcut, calcium_no_flux_boundary, params)
+a_postcut_short, b_postcut_short, c_postcut_short, v_postcut_short = solve_ivp_odes(number_of_cells_postcut, number_of_timepoints_postcut, dt, dx, a_atcut, b_atcut, c_atcut, v_atcut, calcium_no_flux_boundary, params, sample_rate)
 
 end = time.time()
 print("Time consumed solving postcut: ",end - start)
@@ -131,7 +138,7 @@ print("Time consumed solving postcut: ",end - start)
 """ Concatenate pre- and post-cut, adding zeros to replace cut portion """
 
 # the time index '1' here gets rid of the duplicate of t=1.2
-t_postcut = t_postcut[1:]
+t_postcut = store_t_postcut[1:]
 postcut_steps = len(t_postcut)
 
 a_postcut = np.concatenate((np.zeros((left_post_cut,postcut_steps)), a_postcut_short[:,1:], np.zeros((right_post_cut,postcut_steps))))
@@ -139,7 +146,7 @@ b_postcut = np.concatenate((np.zeros((left_post_cut,postcut_steps)), b_postcut_s
 c_postcut = np.concatenate((np.zeros((left_post_cut,postcut_steps)), c_postcut_short[:,1:], np.zeros((right_post_cut,postcut_steps))))
 v_postcut = np.concatenate((np.zeros((left_post_cut,postcut_steps)), v_postcut_short[:,1:], np.zeros((right_post_cut,postcut_steps))))
 
-t = np.concatenate((t_precut, t_postcut))
+t = np.concatenate((store_t_precut, t_postcut))
 t_relative_to_cut = t - cut_time
 
 a = np.concatenate((a_precut, a_postcut),axis=1)
@@ -154,17 +161,11 @@ pickle_directory = save_directory + 'vars/'
 if not os.path.isdir(pickle_directory):
     os.mkdir(pickle_directory)
     
-a_reduced = a[:,::sample_rate]
-b_reduced = b[:,::sample_rate]
-c_reduced = c[:,::sample_rate]
-v_reduced = v[:,::sample_rate]
-t_reduced = t_relative_to_cut[::sample_rate]
-    
-pickle.dump( t_reduced, open(pickle_directory + 't.p','wb') )
-pickle.dump( a_reduced, open(pickle_directory + 'a.p','wb') )
-pickle.dump( b_reduced, open(pickle_directory + 'b.p','wb') )
-pickle.dump( c_reduced, open(pickle_directory + 'c.p','wb') )
-pickle.dump( v_reduced, open(pickle_directory + 'v.p','wb') )
+pickle.dump( t_relative_to_cut, open(pickle_directory + 't.p','wb') )
+pickle.dump( a, open(pickle_directory + 'a.p','wb') )
+pickle.dump( b, open(pickle_directory + 'b.p','wb') )
+pickle.dump( c, open(pickle_directory + 'c.p','wb') )
+pickle.dump( v, open(pickle_directory + 'v.p','wb') )
     
 ########################################################################################
 """ Save code """
@@ -176,12 +177,11 @@ if not os.path.isdir(code_directory):
 filenames = ['make_cut.py', 'params.py', 'models.py', 'plot.py']
 for filename in filenames:
     copy2(filename, code_directory + filename)
-    
 
 ########################################################################################
 """ Create animated output """
 
-create_animated_output(number_of_cells_precut, sample_rate, t_relative_to_cut, a, b, v, c, save_directory)
+create_animated_output(number_of_cells_precut, 1, t_relative_to_cut, a, b, v, c, save_directory)
 
 total_end = time.time()
 print("Total time consumed in working: ",total_end - total_start)
--- models.py
+++ models.py
@@ -5,23 +5,23 @@ from matplotlib.animation import FuncAnimation
 from copy import deepcopy
 
 
-def calcium_periodic_boundary(c, number_of_cells, cell_idx, t_idx):
+def calcium_periodic_boundary(c, number_of_cells, cell_idx):
     
     cell_idx_plus_one = (cell_idx + 1) % number_of_cells
     cell_idx_minus_one = (cell_idx - 1) % number_of_cells
     
-    c_bar = 0.5 * (c[cell_idx_plus_one, t_idx] + c[cell_idx_minus_one, t_idx])
+    c_bar = 0.5 * (c[cell_idx_plus_one] + c[cell_idx_minus_one])
     
     return c_bar
     
-def calcium_no_flux_boundary(c, number_of_cells, cell_idx, t_idx):
+def calcium_no_flux_boundary(c, number_of_cells, cell_idx):
     
     if cell_idx in range(1, number_of_cells - 1):
-        c_bar = (c[cell_idx - 1, t_idx] + c[cell_idx + 1, t_idx]) / 2
+        c_bar = (c[cell_idx - 1] + c[cell_idx + 1]) / 2
     elif cell_idx == 0:
-        c_bar = (c[0, t_idx] + c[1, t_idx]) / 2
+        c_bar = (c[0] + c[1]) / 2
     elif cell_idx == (number_of_cells - 1):
-        c_bar = (c[number_of_cells - 2, t_idx] + c[number_of_cells - 1, t_idx]) / 2
+        c_bar = (c[number_of_cells - 2] + c[number_of_cells - 1]) / 2
     else:
         print('whoops')
         
@@ -32,7 +32,7 @@ def calcium_no_flux_boundary(c, number_of_cells, cell_idx, t_idx):
        
     return c_bar
     
-def solve_ivp_odes(number_of_cells, number_of_timepoints, dt, dx, a0, b0, c0, v0, calcium_boundary_condition, params):
+def solve_ivp_odes(number_of_cells, number_of_timepoints, dt, dx, a0, b0, c0, v0, calcium_boundary_condition, params, sample_rate):
     
     # unpack params
     c_threshold = params["c_threshold"]
@@ -45,41 +45,66 @@ def solve_ivp_odes(number_of_cells, number_of_timepoints, dt, dx, a0, b0, c0, v0
     k_v = params["k_v"]
     mu = params["mu"]
     lambda_const = params["lambda_const"]
-    lambda_const_v = params["lambda_const_v"]
+    lambda_const_v_0 = params["lambda_const_v_0"]
+    lambda_const_v_b = params["lambda_const_v_b"]
+    
+    store_timepoints = int(((number_of_timepoints - 1) / sample_rate) + 1)
+    t = range(number_of_timepoints)
+    store_t = t[::sample_rate]
     
     # Preallocate
-    a = np.ndarray((number_of_cells, number_of_timepoints), dtype=float)
-    b = np.ndarray((number_of_cells, number_of_timepoints), dtype=float)
-    c = np.ndarray((number_of_cells, number_of_timepoints), dtype=float)
-    v = np.ndarray((number_of_cells, number_of_timepoints), dtype=float)
+    a_store = np.ndarray((number_of_cells, store_timepoints), dtype=float)
+    b_store = np.ndarray((number_of_cells, store_timepoints), dtype=float)
+    c_store = np.ndarray((number_of_cells, store_timepoints), dtype=float)
+    v_store = np.ndarray((number_of_cells, store_timepoints), dtype=float)
 
-    H_c = np.ndarray((number_of_cells, number_of_timepoints), dtype=int)
-    H_c_b = np.ndarray((number_of_cells, number_of_timepoints), dtype=int)
-    H_v_b = np.ndarray((number_of_cells, number_of_timepoints), dtype=int)
+    H_c = np.ndarray((number_of_cells), dtype=int)
+    H_c_b = np.ndarray((number_of_cells), dtype=int)
+    H_v_b = np.ndarray((number_of_cells), dtype=int)
 
     # Initialize
-    a[:,0] = a0
-    b[:,0] = b0
-    c[:,0] = c0
-    v[:,0] = v0
-
+    a_store[:,0] = a0
+    b_store[:,0] = b0
+    c_store[:,0] = c0
+    v_store[:,0] = v0
+    
+    a_current = a0
+    b_current = b0
+    c_current = c0
+    v_current = v0
+    
+    a_new = a0
+    b_new = b0
+    c_new = c0
+    v_new = v0
+    
+    store_idx = 1
     for t_idx in range(1, number_of_timepoints):
-        
-        if t_idx % 100000 == 0:
-            print("At time: " + str(t_idx))
 
         for cell_idx in range(number_of_cells):
         
-            c_bar = calcium_boundary_condition(c, number_of_cells, cell_idx, t_idx - 1)
+            c_bar = calcium_boundary_condition(c_current, number_of_cells, cell_idx)
 
-            H_c[cell_idx, t_idx] = np.heaviside(c[cell_idx, t_idx - 1] - c_threshold,1)
-            H_v_b[cell_idx, t_idx] = np.heaviside(v_b_threshold - b[cell_idx, t_idx - 1], 1)
+            H_c[cell_idx] = np.heaviside(c_current[cell_idx] - c_threshold,1)
+            H_v_b[cell_idx] = np.heaviside(v_b_threshold - b_current[cell_idx], 1)
 
-            b[cell_idx, t_idx] = b[cell_idx, t_idx - 1] + dt * ((rho * H_c[cell_idx, t_idx]) - (b[cell_idx, t_idx - 1] * (gamma_0 + (gamma * c[cell_idx, t_idx - 1]))))
-            c[cell_idx, t_idx] = c[cell_idx, t_idx - 1] + dt * ((k * a[cell_idx, t_idx - 1]) + (mu * np.power(1/dx,2) * (c_bar - c[cell_idx, t_idx - 1])) - (lambda_const * c[cell_idx, t_idx - 1]))
-            v[cell_idx, t_idx] = v[cell_idx, t_idx - 1] + dt * ((k_v * H_v_b[cell_idx, t_idx]) - (lambda_const_v * v[cell_idx, t_idx - 1]))
+            b_new[cell_idx] = b_current[cell_idx] + dt * ((rho * H_c[cell_idx]) - (b_current[cell_idx] * (gamma_0 + (gamma * c_current[cell_idx]))))
+            c_new[cell_idx] = c_current[cell_idx] + dt * ((k * a_current[cell_idx]) + (mu * np.power(1/dx,2) * (c_bar - c_current[cell_idx])) - (lambda_const * c_current[cell_idx]))
+            v_new[cell_idx] = v_current[cell_idx] + dt * ((k_v * H_v_b[cell_idx]) - (v_current[cell_idx] * (lambda_const_v_0 + (lambda_const_v_b * b_current[cell_idx]))))
 
-            H_c_b[cell_idx, t_idx] = np.heaviside(c_b_threshold - b[cell_idx, t_idx], 1)
-            a[cell_idx, t_idx] = max(a[cell_idx, t_idx - 1], H_c_b[cell_idx, t_idx])
+            H_c_b[cell_idx] = np.heaviside(c_b_threshold - b_new[cell_idx], 1)
+            a_new[cell_idx] = max(a_current[cell_idx], H_c_b[cell_idx])
             
-    return a, b, c, v
+        if t_idx in store_t:
+            a_store[:,store_idx] = a_new
+            b_store[:,store_idx] = b_new
+            c_store[:,store_idx] = c_new
+            v_store[:,store_idx] = v_new
+            store_idx = store_idx + 1
+            
+        a_current = a_new
+        b_current = b_new
+        c_current = c_new
+        v_current = v_new
+            
+    return a_store, b_store, c_store, v_store

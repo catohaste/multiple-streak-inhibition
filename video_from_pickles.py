4f5fd877e4a57d756b2558de57e5776c25568034
catohaste <catohaste@users.noreply.github.com>
2021-06-09 09:56:12 +0100

    Add file to create output from pickles.

---
+++ video_from_pickles.py
@@ -0,0 +1,24 @@
+import pickle
+
+from plot import create_animated_output
+
+save_directory = 'results/748cells/'
+
+number_of_cells = 748
+
+########################################################################################
+""" Pickle variables """
+
+pickle_directory = save_directory + 'vars/'
+    
+t = pickle.load( open(pickle_directory + 't.p','rb') )
+a = pickle.load( open(pickle_directory + 'a.p','rb') )
+b = pickle.load( open(pickle_directory + 'b.p','rb') )
+c = pickle.load( open(pickle_directory + 'c.p','rb') )
+v = pickle.load( open(pickle_directory + 'v.p','rb') )
+    
+########################################################################################
+""" Create animated output """
+
+create_animated_output(number_of_cells, 1, t, a, b, v, c, save_directory)
+

448c2a3b00fdf76b131ce1bd0025c12e8568b9ac
catohaste <catohastings@gmail.com>
2023-10-13 11:14:26 +0100

    Annotate parameters

--- params.py
+++ params.py
@@ -1,17 +1,17 @@
 params = {
-    "c_threshold": 0.01,
-    "c_b_threshold": 1,
-    'v_b_threshold': 1.22,
-    "rho": 1,
-    "gamma_0": 0.3,
-    "gamma_c": 0.1,
-    "gamma_v": 0.1,
-    "k": 500,
-    "k_v": 5,
-    "mu": 50,
-    "lambda_const": 5,
-    "lambda_const_v_0": 4,
-    "lambda_const_v_b": 0
+    "c_threshold": 0.01,        # calcium act. above threshold -> BMP production (alpha in paper)
+    "c_b_threshold": 1,         # BMP below threshold -> streak (beta_C in paper)
+    'v_b_threshold': 1.22,      # BMP below threshold -> Vg1 production (beta_V in paper)
+    "rho": 1,                   # rate of BMP production (k_B in paper)
+    "gamma_0": 0.3,             # BMP decay (base)
+    "gamma_c": 0.1,             # BMP decay (calcium act. dependent)
+    "gamma_v": 0.1,             # BMP decay (Vg1 dependent)
+    "k": 500,                   # calcium act. production rate, when streak on (k_C in paper)
+    "k_v": 5,                   # Vg1 production rate
+    "mu": 50,                   # diffusion rate (D in paper)
+    "lambda_const": 5,          # calcium act. decay (lambda in paper)
+    "lambda_const_v_0": 4,      # Vg1 decay (mu in paper)
+    "lambda_const_v_b": 0       # Vg1 decay dependent on BMP level (not used)
 }
 
 # params_save_2022_04_18 = {
